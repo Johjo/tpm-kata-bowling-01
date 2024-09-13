3e95c8f8b9566c28ce56e2fff974ea33804f4ed6
Laurent Décamps <decamps.laurent@gmail.com>
2024-09-13 15:54:14 +0200

    feat: add first test, roll method and score for bowling class

--- KataBowling/KataBowlingTest/UnitTest1.cs
+++ KataBowling/KataBowlingTest/UnitTest1.cs
@@ -5,6 +5,26 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        Bowling game = new Bowling();
 
+        for (int i = 0; i < 20; i++)
+        {
+            game.Roll(0);
+        }
+
+        Assert.Equal(game.Score(), 0);
+    }
+}
+
+public class Bowling
+{
+    public void Roll(int i)
+    {
+        
+    }
+
+    public int Score()
+    {
+        return -1;
     }
 }
